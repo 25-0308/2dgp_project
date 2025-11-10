f318cde9662a0a3d478f7a46f8dbda4d4dfe4362
25-0308 <00rhkrtmdwns@hanmail.net>
2025-11-11 00:50:42 +0900

    엔딩 및 캐선창 추가

--- play_mode.py
+++ play_mode.py
@@ -7,7 +7,6 @@ import game_world
 global bg_timer
 
 global bg_count
-global p_l_count
 
 def handle_events():
     event_list = get_events()
@@ -21,9 +20,7 @@ def init():
     global bg
     global bg_count
     global bg_timer
-    global p_l_count
     bg_count = 0
-    p_l_count = 0
     bg_timer = game_framework.frame_time
 
     global canvas_x, canvas_y
@@ -31,14 +28,10 @@ def init():
 
     bg = load_image(f'stage_{bg_count}.png')
 
-    global player_l
-    player_l = load_image(f'kk_{p_l_count}.png')
 
 def update():
     game_world.update()
     global bg_count
-    global p_l_count
-    global player_l
     global bg
     global bg_timer
 
@@ -46,17 +39,15 @@ def update():
     if bg_timer >= 0.15:
         bg_timer = 0.0
         bg_count += 1
-        p_l_count += 1
 
     bg = load_image(f'stage_{bg_count % 8}.png')
-    player_l = load_image(f'kk_{p_l_count % 4}.png')
     game_world.handle_collisions()
 
 def draw():
     clear_canvas()
     bg.clip_composite_draw(0, 0, 752, 224, 0,'0',
                            canvas_x // 2, canvas_y // 2,canvas_x,canvas_y)
-    player_l.clip_composite_draw(0,0,128,224,0,'0',900,200,200,300)
+    #player_l.clip_composite_draw(0,0,128,224,0,'0',900,200,200,300)
     game_world.render()
     update_canvas()
 
