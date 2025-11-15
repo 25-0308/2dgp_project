50d13b8e2254384eeb30ccb887d51bd5ba34b834
25-0308 <00rhkrtmdwns@hanmail.net>
2025-11-15 23:44:05 +0900

    점프추가

--- player_mai.py
+++ player_mai.py
@@ -199,46 +199,46 @@ def load_resource(path):
 #         else:
 #             self.player_kk.image.clip_composite_draw(0, 0, 120, 111, 0, '0',
 #                                                      self.player_kk.x, self.player_kk.y,300,300)
-#
-# class Jump:
-#     def __init__(self, kk):
-#         self.player_kk = kk
-#         self.jump_frame = 0
-#
-#     def enter(self, e):
-#         self.jump_frame = 0
-#         self.player_kk.y = 350
-#
-#
-#     def exit(self, e):
-#         global  jumpattack_k_frame
-#         self.player_kk.dir = 0
-#         jumpattack_k_frame = self.jump_frame
-#
-#     def do(self):
-#         self.jump_frame = (self.jump_frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time * 3) % 14
-#
-#         self.player_kk.load_image(f'kk_jump_sheet.png')
-#
-#         self.player_kk.x += self.player_kk.dir * RUN_SPEED_PPS * game_framework.frame_time
-#
-#         if self.player_kk.x < 50:
-#             self.player_kk.x = 50
-#         elif self.player_kk.x > 1230:
-#             self.player_kk.x = 1230
-#
-#         if self.jump_frame >= 13:
-#             self.player_kk.y = 200
-#             self.player_kk.state_machine.handle_state_event(('TIMEOUT', None))
-#
-#     def draw(self):
-#         if self.player_kk.face_dir == 1:
-#             self.player_kk.image.clip_composite_draw(67 * int(self.jump_frame), 0, 67, 201, 0, 'h',
-#                                                      self.player_kk.x, self.player_kk.y,180,600)
-#         else:
-#             self.player_kk.image.clip_composite_draw(67 * int(self.jump_frame), 0, 67, 201, 0, '0',
-#                                                      self.player_kk.x, self.player_kk.y,180,600)
-#
+
+class Jump:
+    def __init__(self, mai):
+        self.player_mai = mai
+        self.jump_frame = 0
+
+    def enter(self, e):
+        self.jump_frame = 0
+        self.player_mai.y = 350
+
+
+    def exit(self, e):
+        global  jumpattack_k_frame
+        self.player_mai.dir = 0
+        jumpattack_k_frame = self.jump_frame
+
+    def do(self):
+        self.jump_frame = (self.jump_frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time * 3) % 14
+
+        self.player_mai.load_image(f'mai_jump_sprite.png')
+
+        self.player_mai.x += self.player_mai.dir * RUN_SPEED_PPS * game_framework.frame_time
+
+        if self.player_mai.x < 50:
+            self.player_mai.x = 50
+        elif self.player_mai.x > 1230:
+            self.player_mai.x = 1230
+
+        if self.jump_frame >= 13:
+            self.player_mai.y = 200
+            self.player_mai.state_machine.handle_state_event(('TIMEOUT', None))
+
+    def draw(self):
+        if self.player_mai.face_dir == 1:
+            self.player_mai.image.clip_composite_draw(84 * int(self.jump_frame), 0, 84, 210, 0, 'h',
+                                                     self.player_mai.x, self.player_mai.y,180,600)
+        else:
+            self.player_mai.image.clip_composite_draw(84 * int(self.jump_frame), 0, 84, 201, 0, '0',
+                                                     self.player_mai.x, self.player_mai.y,180,600)
+
 class Run:
     def __init__(self, mai):
         self.player_mai = mai
@@ -336,7 +336,7 @@ class Playermai:
 
         self.IDLE = Idle(self)
         self.RUN = Run(self)
-        # self.JUMP = Jump(self)
+        self.JUMP = Jump(self)
         # self.KICK = Kick(self)
         # self.PUNCH = Punch(self)
         # self.JUMPKICK = Jumpkick(self)
@@ -359,10 +359,10 @@ class Playermai:
         self.state_machine = StateMachine(
             self.IDLE,
 {
-            self.IDLE: {right_down: self.RUN, left_down: self.RUN},
+            self.IDLE: {right_down: self.RUN, left_down: self.RUN,up_down: self.JUMP},
             self.RUN: {right_up: self.IDLE, left_up: self.IDLE, right_down: self.IDLE,
-               left_down: self.IDLE},
-            # self.JUMP: {time_out: self.IDLE,
+               left_down: self.IDLE,up_down: self.JUMP},
+            self.JUMP: {time_out: self.IDLE},
             #             (lambda e, jj=self.JUMP: k_down(e) and jj.jump_frame < 7): self.JUMPKICK},
             # self.JUMPKICK: {time_out: self.IDLE},
             # self.KICK: {time_out: self.IDLE},
