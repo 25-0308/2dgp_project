50ad9e0186cf27273e5094abab5832c581e37478
25-0308 <00rhkrtmdwns@hanmail.net>
2025-12-06 18:41:20 +0900

    사운드 관리 파일 추가

---
+++ sound_files.py
@@ -0,0 +1,7 @@
+SOUND_FILES = {
+    'punch': 'resource/sound/punch.wav',
+    'kick': 'resource/sound/kick.wav',
+    'skill1': 'resource/sound/skill1.wav',
+    'skill2': 'resource/sound/skill2.wav',
+    'hit': 'resource/sound/hit.wav',
+}
--- sound_manager.py
+++ sound_manager.py
@@ -12,18 +12,15 @@ class SoundManager:
         return cls._instance
 
     def load(self, name, file_path):
-        """사운드 파일을 로드하고 딕셔너리에 저장합니다."""
         if name not in self.sounds:
             self.sounds[name] = load_wav(file_path)
             print(f'[SoundManager] Loaded: {name} from {file_path}')
 
     def play(self, name):
-        """저장된 사운드를 재생합니다."""
         if name in self.sounds:
             # 볼륨 조절이 필요하다면 self.sounds[name].set_volume(64) 와 같이 사용 (0~128)
             self.sounds[name].play()
         else:
             print(f'[SoundManager] Warning: Sound not found - {name}')
 
-# 어디서든 쉽게 접근할 수 있도록 전역 인스턴스 생성
 sound_manager = SoundManager()
